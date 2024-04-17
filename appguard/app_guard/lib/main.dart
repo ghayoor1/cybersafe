5e208616d997f959dcc5b75e85cfa3667b4a203b
Raza <muhammad.raz@ascendcorp.com>
2024-04-17 22:13:41 +0500

    changed name to App Guard

--- appguard/app_guard/lib/main.dart
+++ appguard/app_guard/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Device Info App',
+      title: 'App Guard',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
